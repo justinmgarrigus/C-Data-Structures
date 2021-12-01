4a91311c6966f4936eac69d26c46adea1b258793
justinmgarrigus <justin.m.garrigus@gmail.com>
2021-12-01 01:06:46 -0600

    Implemented generic types for vectors.

--- lib/vector.c
+++ lib/vector.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include "vector.h"
 
-vector* vector_create(int initial_capacity) {
+vector* vector_create(size_t initial_capacity, size_t data_size) {
 	vector *v = malloc(sizeof(vector)); 
-	v->data = malloc(sizeof(v->data) * initial_capacity);
+	v->data = malloc(data_size * initial_capacity);
+	v->data_size = data_size; 
 	v->count = 0;
 	v->capacity = initial_capacity; 
 }
@@ -17,34 +18,46 @@ void vector_destroy(vector *v) {
 	free(v); 
 }
 
-void vector_double_capacity(vector *v) {
-	v->data = realloc(v->data, sizeof(v->data) * v->capacity * 2);
+void vector_increase_capacity(vector *v) {
+	v->data = realloc(v->data, v->data_size * v->capacity * 2);
 	v->capacity *= 2; 
 }
 
-void vector_add(vector *v, int item) {
-	if (v->count == v->capacity) {
-		vector_double_capacity(v); 
-	}
-
-	v->data[v->count++] = item; 
+void vector_set(vector *v, unsigned long item, size_t index) {
+	// Bytes stored in little-endian order. 
+	// Treat 'data' is if it were a byte array.
+	int current_index = index * v->data_size; 
+	for (int i = 0; i < v->data_size; i++) 
+		*((unsigned char*)v->data + current_index + i) = item >> 8 * i * 255; 
 }
 
-int vector_set(vector *v, int item, int index) {
-	if (index >= v->capacity)
-		return -1; 
-	v->data[index] = item; 
+void vector_add(vector *v, unsigned long item) {
+	if (v->count == v->capacity) {
+		vector_increase_capacity(v); 
+	}
+
+	vector_set(v, item, v->count++); 
+}
+
+unsigned long vector_get(vector *v, size_t index) {
+	// Construct data type of size 'buffer_size', bit-shifting bytes over
+	// from data buffer. 
+	unsigned long result = 0; 
+	int current_index = index * v->data_size;
+	for (int i = 0; i < v->data_size; i++) 
+		result |= *((unsigned char*)v->data + current_index + i) << 8 * i; 
+	return result; 
 }
 
 void vector_string(vector *v, char buffer[]) {
 	buffer[0] = '['; 
 	int buffer_index = 1; 
-	char int_string[12]; // min value of integer as a string (11 digits + null char) 
+	char int_string[11]; // max value of unsigned long as a string (10 digits + null char) 
 	buffer[0] = '['; 
 	int item_index = 0; 
 	int str_index; 
 	while (item_index < v->capacity) {
-		snprintf(int_string, 12, "%d", v->data[item_index++]);
+		snprintf(int_string, 11, "%lu", vector_get(v, item_index++));
 		str_index = 0;
 		while (int_string[str_index] != '\0') 
 			buffer[buffer_index++] = int_string[str_index++]; 
@@ -56,27 +69,30 @@ void vector_string(vector *v, char buffer[]) {
 	buffer[buffer_index] = '\0'; 
 }
 
-int main() {
-	char buffer[100]; 
-	vector *v = vector_create(5); 
-	
-	vector_add(v, 1);
-	vector_add(v, 2);
-	vector_add(v, 3); 
-	vector_string(v, buffer);
-	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-	
-	vector_add(v, 4);
-	vector_add(v, 5);
-	vector_add(v, 6);
-	vector_string(v, buffer);
-	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-	
-	vector_set(v, 7, 0);
-	vector_set(v, 8, 1);
-	vector_set(v, 9, 2);
-	vector_string(v, buffer);
-	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-
-	vector_destroy(v); 
-}
+//int main() {
+//	char buffer[100]; 
+//	vector *v = vector_create(8, sizeof(int)); 
+//	
+//	vector_add(v, 1);
+//	vector_add(v, 2);
+//	vector_add(v, 3); 
+//	vector_string(v, buffer);
+//	printf("%lu, %lu: %s\n", v->count, v->capacity, buffer); 
+//	
+//	vector_add(v, 4);
+//	vector_add(v, 5);
+//	vector_add(v, 6);
+//	vector_string(v, buffer);
+//	printf("%lu, %lu: %s\n", v->count, v->capacity, buffer); 
+//	
+//	vector_set(v, 7, 0);
+//	vector_set(v, 8, 1);
+//	vector_set(v, 9, 2);
+//	vector_string(v, buffer);
+//	printf("%lu, %lu: %s\n", v->count, v->capacity, buffer); 
+//
+//	for (int i = 0; i < v->count; i++)
+//		printf("Item at {%d}: {%d}\n", i, (int)vector_get(v, i)); 
+//
+//	vector_destroy(v); 
+//}
--- lib/vector.h
+++ lib/vector.h
@@ -1,13 +1,15 @@
 // Justin Garrigus, 11-30-21
 
 typedef struct vector { 
-	int capacity; 
-	int count;
-	int *data;
+	size_t capacity; 
+	size_t count;
+	size_t data_size; 
+	void *data;
 } vector;
 
-vector* vector_create(int initial_capacity);
+vector* vector_create(size_t initial_capacity, size_t data_size);
 void vector_destroy(vector *v);
-void vector_add(vector *v, int item);
-int vector_set(vector *v, int item, int index);
+void vector_set(vector *v, unsigned long item, size_t index); 
+void vector_add(vector *v, unsigned long item);
+unsigned long vector_get(vector *v, size_t index); 
 void vector_string(vector *v, char buffer[]); 

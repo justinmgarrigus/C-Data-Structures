55ff299bb267aeb1816031b67fcecd0385f54705
justinmgarrigus <justin.m.garrigus@gmail.com>
2021-11-30 21:51:58 -0600

    Added typedef for vector.

--- lib/vector.c
+++ lib/vector.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include "vector.h"
 
-struct vector* vector_create(int initial_capacity) {
-	struct vector *v = malloc(sizeof(struct vector)); 
+vector* vector_create(int initial_capacity) {
+	vector *v = malloc(sizeof(vector)); 
 	v->data = malloc(sizeof(v->data) * initial_capacity);
 	v->count = 0;
 	v->capacity = initial_capacity; 
@@ -17,12 +17,12 @@ void vector_destroy(vector *v) {
 	free(v); 
 }
 
-void vector_double_capacity(struct vector *v) {
+void vector_double_capacity(vector *v) {
 	v->data = realloc(v->data, sizeof(v->data) * v->capacity * 2);
 	v->capacity *= 2; 
 }
 
-void vector_add(struct vector *v, int item) {
+void vector_add(vector *v, int item) {
 	if (v->count == v->capacity) {
 		vector_double_capacity(v); 
 	}
@@ -30,13 +30,13 @@ void vector_add(struct vector *v, int item) {
 	v->data[v->count++] = item; 
 }
 
-int vector_set(struct vector *v, int item, int index) {
+int vector_set(vector *v, int item, int index) {
 	if (index >= v->capacity)
 		return -1; 
 	v->data[index] = item; 
 }
 
-void vector_string(struct vector *v, char buffer[]) {
+void vector_string(vector *v, char buffer[]) {
 	buffer[0] = '['; 
 	int buffer_index = 1; 
 	char int_string[12]; // min value of integer as a string (11 digits + null char) 
@@ -56,27 +56,27 @@ void vector_string(struct vector *v, char buffer[]) {
 	buffer[buffer_index] = '\0'; 
 }
 
-//int main() {
-//	char buffer[100]; 
-//	vector *v = vector_create(5); 
-//	
-//	vector_add(v, 1);
-//	vector_add(v, 2);
-//	vector_add(v, 3); 
-//	vector_string(v, buffer);
-//	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-//	
-//	vector_add(v, 4);
-//	vector_add(v, 5);
-//	vector_add(v, 6);
-//	vector_string(v, buffer);
-//	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-//	
-//	vector_set(v, 7, 0);
-//	vector_set(v, 8, 1);
-//	vector_set(v, 9, 2);
-//	vector_string(v, buffer);
-//	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
-//
-//	vector_destroy(v); 
-//}
+int main() {
+	char buffer[100]; 
+	vector *v = vector_create(5); 
+	
+	vector_add(v, 1);
+	vector_add(v, 2);
+	vector_add(v, 3); 
+	vector_string(v, buffer);
+	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
+	
+	vector_add(v, 4);
+	vector_add(v, 5);
+	vector_add(v, 6);
+	vector_string(v, buffer);
+	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
+	
+	vector_set(v, 7, 0);
+	vector_set(v, 8, 1);
+	vector_set(v, 9, 2);
+	vector_string(v, buffer);
+	printf("%d, %d: %s\n", v->count, v->capacity, buffer); 
+
+	vector_destroy(v); 
+}
